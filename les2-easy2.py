394e46c4bfa59e5601eb17f3377f8144a99a861a
Impactos <salomatin.ilia@gmail.com>
2018-09-30 11:16:34 +0300

    2

--- les2-easy2.py
+++ les2-easy2.py
@@ -1,4 +1,3 @@
-
 import random
 
 list1 = [random.randrange(1, 7), random.randrange(1, 7), random.randrange(1, 7), random.randrange(1, 7), random.randrange(1, 7)]
@@ -7,14 +6,11 @@ list2 = [random.randrange(1, 7), random.randrange(1, 7), random.randrange(1, 7),
 print(list1)
 print(list2)
 
-list3 = []
+for r in reversed(list1):
+    if list2.count(r)>0:
+        list1.remove(r)
 
-for r in list1:
-    if list2.count(r) == 0:
-        list3.append(r)
-
-if list3 == []:
+if list1 == []:
     print ("В первом списке все числа совпадают с числами из второго списка")
 else:
-    print ("Осталось:", list3)
-
+    print ("Осталось:", list1)
