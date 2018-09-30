eaee3da098a2436a0c051ef04e6db6d0ac8fc1f8
Impactos <salomatin.ilia@gmail.com>
2018-09-30 11:03:11 +0300

    1

---
+++ les2-easy1.py
@@ -0,0 +1,10 @@
+fruitlist = ["яблоко", "банан", "киви", "арбуз"]
+print("Дано: ", fruitlist)
+print("Вывод:")
+
+n = 1
+
+for c in fruitlist:
+    print(str(n) + ".", "{:>7}".format(c))
+    n += 1
+
