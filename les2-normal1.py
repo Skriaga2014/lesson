511b3195fe026636313828cd428ddb27d2065f5c
Impactos <salomatin.ilia@gmail.com>
2018-09-30 13:31:36 +0300

    1

---
+++ les2-normal1.py
@@ -0,0 +1,20 @@
+import random
+import math
+
+list1 = []
+list2 = []
+
+a = 0
+while a < random.randrange(10,20):
+    list1.append(random.randrange(1,100))
+    a += 1
+print ("Дано:", list1)
+
+for i in list1:
+    if math.sqrt(i)%1 == 0:
+        list2.append(int(math.sqrt(i)))
+
+if list2 == []:
+    print("В списке нет чисел, из которых можно извлечь корень")
+else:
+    print ("Результат:", list2)
