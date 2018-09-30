0327e87af281d61abb19cd8de3861d76a02809b9
Impactos <salomatin.ilia@gmail.com>
2018-09-30 11:03:23 +0300

    3

---
+++ les2-easy3.py
@@ -0,0 +1,16 @@
+import random
+
+list1 = []
+for i in range(0, 10):
+    list1.append(random.randrange(1, 10))
+print(list1)
+
+n = 0
+for r in list1:
+    if r%2 == 0:
+        list1[n] = r/4
+    else:
+        list1[n] = r*2
+    n += 1
+print(list1)
+
