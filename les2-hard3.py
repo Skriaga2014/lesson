7c02d7cdef5aae5c82cfa1bec37aeb64d30d999b
Impactos <salomatin.ilia@gmail.com>
2018-10-01 23:45:20 +0300

    33

--- les2-hard3.py
+++ les2-hard3.py
@@ -1,7 +1,9 @@
-tower = []
-sector = 1
-spos = 0
-for i in range(1,100):
+tower = []  #список списков
+sector = 1  #сектор комнат (Сектор 4 - 4 этажа по 4 комнаты)
+spos = 0    #номер комнаты в секторе (номер вышел за пределы сектора - новый сектор)
+
+
+for i in range(1,100):      #100 - количество комнат в доме
 
     if tower == []:
         tower.append([i])
@@ -23,13 +25,17 @@ for i in range(1,100):
             spos = 1
 
 
+
+
 for r in reversed(tower):
-    print (r)
+    ttt = len(str(tower.index(r)+1))                                #длина номера этажа
+    rrr = (len(str(tower[-2]))-len(str(tower[tower.index(r)])))//2  #половина разницы между длины самой длинной строки и текущей строки
+    print (str(tower.index(r)+1) + "." * (5-ttt+rrr), r)
 
 num = 0
 floor = 0
 
-room = int(input("Введите номер комнаты: "))
+room = int(input("\n Введите номер комнаты: "))
 for t in tower:
     if t.count(room) == 1:
         floor = tower.index(t)+1
