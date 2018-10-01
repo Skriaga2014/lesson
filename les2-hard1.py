a283aef364073cb87192c42443d95568db8e2b1c
Impactos <salomatin.ilia@gmail.com>
2018-10-01 23:45:26 +0300

    1

---
+++ les2-hard1.py
@@ -0,0 +1,22 @@
+
+equation = "y = -12x + 11111140.2121"
+x = 2.5
+
+print (equation)
+print ("x =", x)
+
+a = equation.split()
+a[2] = a[2][:-1]
+
+a[2] = float(a[2])
+
+if a[3] == "-":
+    a[3] = "+"
+    a[4] = -float(a[4])
+else:
+    a[4] = float(a[4])
+
+y = a[2] * x + a[4]
+
+print("\n", a, "\n")
+print("y =", y)
